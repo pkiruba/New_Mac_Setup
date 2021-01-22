ca37a7935b24dd2ae9cd9248e3985da239a44710
pkiruba <kirubanpk@gmail.com>
2021-01-21 19:48:55 -0500

    New Mac Setup Tools

---
+++ brew_package_install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install tree
+
+# Install MacOS Applications (for removing applications - brew cask uninstall firefox)
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask discord
+brew install --cask zoom
+brew install --cask skype
+brew install --cask dropbox
+brew install --cask onedrive
+brew install --cask slack
+
